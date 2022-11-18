3d043a007e46c78987dfe700503e6c518c50b428
manipriyan77 <manipriyangopalan@gmail.com>
2022-11-18 21:40:49 +0530

    finished vertical Stepper

--- src/Components/VerticalStepper/VerticalStepper.style.tsx
+++ src/Components/VerticalStepper/VerticalStepper.style.tsx
@@ -1,7 +1,212 @@
 import styled from "styled-components";
 
 export const VerticalStepperWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  /* align-items: center; */
+  width: 40%;
+  margin: auto;
+`;
+
+export const ButtonWrapper = styled.div``;
+
+export const ButtonContained = styled.button`
+  display: -webkit-inline-box;
+  display: -webkit-inline-flex;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
   align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  position: relative;
+  box-sizing: border-box;
+  -webkit-tap-highlight-color: transparent;
+  background-color: transparent;
+  outline: 0;
+  border: 0;
+  margin: 0;
+  border-radius: 0;
+  padding: 0;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  vertical-align: middle;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  -webkit-text-decoration: none;
+  text-decoration: none;
+  color: inherit;
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 1.75;
+  letter-spacing: 0.02857em;
+  text-transform: uppercase;
+  min-width: 64px;
+  padding: 6px 16px;
+  border-radius: 4px;
+  -webkit-transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  color: #fff;
+  background-color: #1976d2;
+  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%),
+    0px 1px 5px 0px rgb(0 0 0 / 12%);
+  margin-top: 8px;
+  margin-right: 8px;
+`;
+
+export const ButtonTransparent = styled.button`
+  display: -webkit-inline-box;
+  display: -webkit-inline-flex;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  position: relative;
+  box-sizing: border-box;
+  -webkit-tap-highlight-color: transparent;
+  background-color: transparent;
+  outline: 0;
+  border: 0;
+  margin: 0;
+  border-radius: 0;
+  padding: 0;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  vertical-align: middle;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  -webkit-text-decoration: none;
+  text-decoration: none;
+  color: inherit;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 1.75;
+  letter-spacing: 0.02857em;
+  text-transform: uppercase;
+  min-width: 64px;
+  padding: 6px 8px;
+  border-radius: 4px;
+  -webkit-transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  color: #1976d2;
+  margin-top: 8px;
+  margin-right: 8px;
+`;
+
+export const StepperText = styled.div`
+  margin-left: 12px;
+  padding-left: 20px;
+  padding-right: 8px;
+  border-left: 1px solid #bdbdbd;
+`;
+
+export const StepperHeadingWrapper = styled.div`
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  text-align: left;
+  padding: 8px 0;
+`;
+
+export const CircleNumber = styled.circle`
+  width: 20px;
+  height: 20px;
+  background-color: #1976d2;
+  border-radius: 50px;
+  color: white;
+  font-size: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StepperHeading = styled.span`
+  margin-left: 10px;
+`;
+
+export const ButtonTransparentDisabled = styled.button`
+  display: -webkit-inline-box;
+  display: -webkit-inline-flex;
+  display: -ms-inline-flexbox;
+  display: inline-flex;
+  -webkit-align-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  position: relative;
+  box-sizing: border-box;
+  -webkit-tap-highlight-color: transparent;
+  background-color: transparent;
+  outline: 0;
+  border: 0;
+  margin: 0;
+  border-radius: 0;
+  padding: 0;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  vertical-align: middle;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  -webkit-text-decoration: none;
+  text-decoration: none;
+  color: inherit;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 1.75;
+  letter-spacing: 0.02857em;
+  text-transform: uppercase;
+  min-width: 64px;
+  padding: 6px 8px;
+  border-radius: 4px;
+  -webkit-transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  margin-top: 8px;
+  margin-right: 8px;
+  color: grey;
 `;
--- src/Components/VerticalStepper/VerticalStepper.tsx
+++ src/Components/VerticalStepper/VerticalStepper.tsx
@@ -1,35 +1,58 @@
 import React, { useState } from "react";
-import { VerticalStepperWrapper } from "./VerticalStepper.style";
+import {
+  VerticalStepperWrapper,
+  ButtonWrapper,
+  ButtonContained,
+  ButtonTransparent,
+  StepperText,
+  StepperHeadingWrapper,
+  CircleNumber,
+  StepperHeading,
+  ButtonTransparentDisabled,
+} from "./VerticalStepper.style";
 
 const VerticalStepper = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [stepperCount, setStepperCount] = useState<number>(1);
+
   const array = ["First", "Second", "Third", "Fourth", "Fifth"];
   const textArrays = [
     "For each ad campaign that you create, you can control how much you're willing to spend on clicks and conversions, which networks and geographical locations you want your ads to show on, and more. ",
     "An ad group contains one or more ads which target a shared set of keywords.    ",
     "Try out different ad text to see what brings in the most customers, and learn how to enhance your ads using features like ad extensions. If you run into any problems with your ads, find out how to tell if they're running and how to resolve approval issues.    ",
+    "Try out different ad text to see what brings in the most customers, and learn how to enhanur ads, find out how to tell if they're running and how to resolve approval issues.    ",
   ];
 
   return (
-    <>
-      <VerticalStepperWrapper>
-        {array.map((text, index) => {
-          return (
-            <div>
-              <div>
-                <span>{index + 1}</span>
-                {text}
-              </div>
-              <div>
-                {textArrays.map((ele, index) => {
-                  return <div>{isOpen && ele}</div>;
-                })}
-              </div>
-            </div>
-          );
-        })}
-      </VerticalStepperWrapper>
-    </>
+    <VerticalStepperWrapper>
+      {textArrays.map((txt, i) => {
+        return (
+          <>
+            <StepperHeadingWrapper
+              className={`${stepperCount === i + 1 && "active"} 
+              ${i + 1 > stepperCount && "disable"} `}
+            >
+              <CircleNumber> {i + 1} </CircleNumber>
+              <StepperHeading> {array[i]}</StepperHeading>
+            </StepperHeadingWrapper>
+            <StepperText>{stepperCount === i + 1 && txt}</StepperText>
+            {stepperCount === i + 1 && (
+              <ButtonWrapper>
+                <ButtonContained onClick={() => setStepperCount((prev) => prev + 1)}>
+                  {i === textArrays.length - 1 ? "Finish" : "Continue"}
+                </ButtonContained>
+                {stepperCount === 1 ? (
+                  <ButtonTransparentDisabled disabled>Back</ButtonTransparentDisabled>
+                ) : (
+                  <ButtonTransparent onClick={() => setStepperCount((prev) => prev - 1)}>
+                    Back
+                  </ButtonTransparent>
+                )}
+              </ButtonWrapper>
+            )}
+          </>
+        );
+      })}
+    </VerticalStepperWrapper>
   );
 };
 
